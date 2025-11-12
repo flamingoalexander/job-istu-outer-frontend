277c6340b6b762ac01af1be45113434aa29dd317
shadowofcold <mdelizov@mail.ru>
2025-11-12 18:25:20 +0800

    Исправление замечаний по поводу разметки, стилей и скрипта

--- src/pages/ListOfInternshipsPage.vue
+++ src/pages/ListOfInternshipsPage.vue
@@ -1,171 +1,150 @@
 <template>
   <q-page class="q-pa-md bg-grey-2">
     <div class="row q-col-gutter-md">
-      <!-- Левая часть с фильтрами -->
-
       <div class="col-12 col-md-3">
-        <div class="filter-card-wrapper">
-          <q-card flat bordered class="filter-card q-pa-md">
-            <div class="filter-title q-mb-lg">Фильтры</div>
+        <q-card
+          flat
+          bordered
+          class="text-white q-pa-md primary-background-card-color"
+          style="border-radius: 10px"
+        >
+          <div class="q-mb-sm" style="font-size: 20px; font-weight: 700">Фильтры</div>
 
-            <!-- Компания -->
-            <div class="q-mb-md">
-              <label class="filter-label">Компания</label>
-              <q-input
-                filled
-                dense
-                v-model="filters.company"
-                placeholder="Имя компании"
-                class="input-blue"
-                hide-bottom-space
-              />
-            </div>
+          <div class="q-mb-md">
+            <div class="text-subtitle2 q-mb-xs">Компания</div>
+            <q-input
+              filled
+              dense
+              v-model="filters.company"
+              bg-color="white"
+              color="primary"
+              hide-bottom-space
+              label="Имя компании"
+              label-color="primary"
+            />
+          </div>
 
-            <!-- Специальности -->
-            <div class="q-mb-md">
-              <label class="filter-label">Специальности</label>
-              <q-select
-                filled
-                dense
-                v-model="filters.specialities"
-                :options="specialities"
-                multiple
-                emit-value
-                map-options
-                use-chips
-                label="Выберите специальности"
-                class="select-blue"
-                hide-bottom-space
-              />
-            </div>
+          <div class="q-mb-md">
+            <div class="text-subtitle2 q-mb-xs">Специальности</div>
+            <q-select
+              filled
+              dense
+              v-model="filters.specialities"
+              :options="specialities"
+              multiple
+              emit-value
+              map-options
+              use-chips
+              label="Выберите специальности"
+              label-color="primary"
+              bg-color="white"
+              hide-bottom-space
+            />
+          </div>
 
-            <!-- Навыки -->
-            <div>
-              <label class="filter-label">Навыки</label>
-              <q-select
-                filled
-                dense
-                v-model="filters.skills"
-                :options="skills"
-                multiple
-                emit-value
-                map-options
-                use-chips
-                label="Выберите навыки"
-                class="select-blue"
-                hide-bottom-space
-              />
-            </div>
-          </q-card>
-        </div>
+          <div>
+            <div class="text-subtitle2 q-mb-xs">Навыки</div>
+            <q-select
+              filled
+              dense
+              v-model="filters.skills"
+              :options="skills"
+              multiple
+              emit-value
+              map-options
+              use-chips
+              label="Выберите навыки"
+              label-color="primary"
+              bg-color="white"
+              hide-bottom-space
+            />
+          </div>
+        </q-card>
       </div>
 
-      <!-- Правая часть с карточками практик -->
       <div class="col-12 col-md-9">
-        <div class="q-mb-md text-md-left">
+        <div class="q-mb-sm">
           <q-btn
             label="Я сам нашел компанию"
             color="primary"
             text-color="white"
             unelevated
-            class="btn-personal-internship"
+            class="q-pa-sm"
+            style="font-weight: 700; border-radius: 10px"
           />
         </div>
 
-        <div
-          v-for="(internship, index) in filteredInternships"
-          :key="'internship-' + index"
-          class="q-mb-md"
+        <q-card
+          v-for="internship in filteredInternships"
+          :key="internship.title"
+          flat
+          class="q-mb-sm row items-center justify-between text-white primary-background-card-color"
+          style="border-radius: 10px"
         >
-          <q-card flat bordered class="internship-card">
-            <q-card-section>
-              <div class="row q-col-gutter-md items-start text-white">
-                <div class="col">
-                  <div class="card-title">
-                    {{ internship.title }}
-                  </div>
+          <q-card-section class="col-12 col-md-8">
+            <div style="font-size: 20px; font-weight: 700">{{ internship.title }}</div>
+            <div class="q-mt-xs">
+              <strong>Специальности: </strong>
+              <span>{{ internship.specialities.join(', ') }}</span>
+            </div>
 
-                  <div class="card-line q-mt-xs">
-                    <strong>Специальности: </strong>
-                    <span class="card-text"> {{ internship.specialities.join(', ') }}</span>
-                  </div>
-
-                  <div class="card-line q-mt-xs row items-center no-wrap">
-                    <strong class="q-mr-sm">Навыки:</strong>
-                    <div class="row items-center q-gutter-xs wrap">
-                      <q-chip
-                        v-for="(skill, sindex) in internship.skills"
-                        :key="sindex"
-                        outline
-                        text-color="white"
-                        color="white"
-                        class="q-mr-xs q-chip-skills"
-                      >
-                        {{ skill }}
-                      </q-chip>
-                    </div>
-                  </div>
-                </div>
-
-                <div class="col-auto text-right">
-                  <div class="row items-center justify-end no-wrap">
-                    <div class="company-name q-mr-sm">
-                      {{ internship.company }}
-                    </div>
-                    <q-avatar class="avatar-white">
-                      <div class="avatar-picture"></div>
-                    </q-avatar>
-                  </div>
-
-                  <div class="q-mt-sm justify-end">
-                    <q-btn
-                      label="Подробнее"
-                      color="white"
-                      text-color="#1976d2"
-                      unelevated
-                      class="btn-detailed"
-                    />
-                  </div>
-                </div>
+            <div class="q-mt-xs row items-center">
+              <strong class="q-mr-sm">Навыки:</strong>
+              <div class="q-mt-auto q-gutter-xs flex-wrap">
+                <q-chip v-for="skill in internship.skills" :key="skill" outline color="white" dense>
+                  {{ skill }}
+                </q-chip>
               </div>
-            </q-card-section>
-          </q-card>
-        </div>
+            </div>
+          </q-card-section>
+
+          <q-card-section class="col-12 col-md-3 text-right">
+            <div class="row items-center justify-end q-gutter-xs">
+              <div style="font-size: 16px; font-weight: 700">{{ internship.company }}</div>
+              <q-avatar size="40px" color="white"></q-avatar>
+            </div>
+
+            <div class="q-mt-sm">
+              <q-btn
+                label="Подробнее"
+                color="white"
+                text-color="primary"
+                unelevated
+                style="font-weight: 700"
+              />
+            </div>
+          </q-card-section>
+        </q-card>
       </div>
     </div>
   </q-page>
 </template>
 
 <script lang="ts" setup>
-import { ref, computed } from 'vue';
+import { reactive, computed } from 'vue';
+import { filter, some, includes, toLower } from 'lodash';
+import type { Internship } from 'src/types';
 
-interface Internship {
-  title: string;
-  company: string;
-  specialities: string[];
-  skills: string[];
-}
-
-const filters = ref({
+const filters = reactive({
   company: '',
   specialities: [] as string[],
   skills: [] as string[],
 });
 
-const specialities = [
+const specialities = reactive([
   { label: 'ИСиС', value: 'ИСиС' },
   { label: 'АСУ', value: 'АСУ' },
   { label: 'ЭВМБ', value: 'ЭВМБ' },
   { label: 'ИБС', value: 'ИБС' },
-];
+]);
 
-const skills = [
+const skills = reactive([
   { label: 'Python', value: 'Python' },
   { label: 'Fast API', value: 'Fast API' },
   { label: 'Веб-программирование', value: 'Веб-программирование' },
-];
+]);
 
-const internships = ref<Internship[]>([
+const internships = reactive<Internship[]>([
   {
     title: 'Разработка микросервиса на Fast API',
     company: 'Какая-то компания',
@@ -186,152 +165,26 @@ const internships = ref<Internship[]>([
   },
 ]);
 
-const filteredInternships = computed(() =>
-  internships.value.filter((internship) => {
+const filteredInternships = computed(() => {
+  return filter(internships, (internship: Internship) => {
     const matchCompany =
-      !filters.value.company ||
-      internship.company.toLowerCase().includes(filters.value.company.toLowerCase());
+      !filters.company || includes(toLower(internship.company), toLower(filters.company));
 
     const matchSpecialities =
-      filters.value.specialities.length === 0 ||
-      filters.value.specialities.some((sp) => internship.specialities.includes(sp));
+      filters.specialities.length === 0 ||
+      some(filters.specialities, (sp: string) => includes(internship.specialities, sp));
 
     const matchSkills =
-      filters.value.skills.length === 0 ||
-      filters.value.skills.some((sk) => internship.skills.includes(sk));
+      filters.skills.length === 0 ||
+      some(filters.skills, (sk: string) => includes(internship.skills, sk));
 
     return matchCompany && matchSpecialities && matchSkills;
-  }),
-);
+  });
+});
 </script>
 
-<style scoped>
-/* Левая часть с фильтрами */
-.filter-card-wrapper {
-  position: sticky;
-  top: 67px; /* расстояние от верха страницы */
-}
-
-.filter-card {
-  background-color: #1976d2;
-  border-radius: 10px;
-  padding: 16px;
-  color: #ffffff;
-}
-
-.filter-title {
-  font-size: 20px;
-  font-weight: 700;
-  color: #ffffff;
-  margin-bottom: 16px;
-}
-
-.filter-label {
-  display: block;
-  margin-bottom: 6px;
-  color: #ffffff;
-  font-weight: 600;
-  font-size: 16px;
-}
-
-.input-blue ::v-deep(.q-field__control) {
-  background-color: #ffffff !important;
-  border-radius: 6px;
-}
-
-.input-blue ::v-deep(.q-field__native) {
-  color: #1976d2 !important;
-}
-
-.input-blue ::v-deep(input::placeholder) {
-  color: #1976d2 !important;
-  opacity: 1;
-}
-
-.select-blue ::v-deep(.q-field__control) {
-  background-color: #ffffff !important;
-  border-radius: 6px;
-}
-
-.select-blue ::v-deep(.q-field__label) {
-  color: #1976d2 !important;
-}
-
-.select-blue ::v-deep(.q-chip) {
-  background-color: rgba(25, 118, 210, 0.1) !important;
-  color: #1976d2 !important;
-  border: 1px solid rgba(25, 118, 210, 0.3) !important;
-}
-
-/* Правая часть с карточками практик */
-.internship-card {
-  background-color: #1976d2;
-  border-radius: 10px;
-}
-
-.card-title {
-  font-size: 20px;
-  font-weight: 700;
-  line-height: 1.2;
-}
-
-.card-text,
-.card-line {
-  font-size: 14px;
-}
-
-.company-name {
-  font-size: 20px;
-  font-weight: 600;
-}
-
-.avatar-white {
-  background-color: rgba(255, 255, 255, 0.12);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-radius: 50%;
-  width: 40px;
-  height: 40px;
-}
-
-.avatar-picture {
-  font-size: 14px;
-}
-
-.btn-detailed {
-  font-size: 14px;
-  font-weight: 600;
-  border-radius: 8px;
-  color: #1976d2;
-}
-
-.btn-personal-internship {
-  font-weight: 600;
-  border-radius: 8px;
-  color: #1976d2;
-}
-
-.q-chip-skills {
-  font-size: 14px;
-  padding: 2px 10px;
-}
-
-/* Адаптация под мобильные устройства */
-@media (max-width: 750px) {
-  .card-title,
-  .company-name {
-    font-size: 12px;
-  }
-  .card-text,
-  .card-line,
-  .q-chip-skills,
-  .btn-detailed {
-    font-size: 10px;
-  }
-  .avatar-white {
-    width: 20px;
-    height: 20px;
-  }
+<style lang="scss" scoped>
+.primary-background-card-color {
+  background-color: $primary;
 }
 </style>
