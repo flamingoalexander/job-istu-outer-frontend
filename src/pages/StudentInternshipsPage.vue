d40a519044d681cf389d06e9a7639e8cbbd12533
shadowofcold <mdelizov@mail.ru>
2025-11-26 17:24:56 +0800

    Выделение компонентов в отдельную папку и добавление проверки на остутствие активной практики

--- src/pages/StudentInternshipsPage.vue
+++ src/pages/StudentInternshipsPage.vue
@@ -2,227 +2,34 @@
   <q-page class="q-pa-xl">
     <div class="text-h3 q-mb-lg text-weight-bold">Мои практики</div>
 
-    <div class="text-h4 q-mb-sm">Текущая</div>
+    <template v-if="currentInternship">
+      <div class="text-h4 q-mb-sm text-weight-bold">Текущая</div>
+      <CurrentInternship v-model:internship="currentInternship" />
+    </template>
 
-    <q-card class="q-mb-lg primary-background border-radius-md" flat bordered>
-      <div class="row justify-between">
-        <q-card-section class="col-12 col-md-8">
-          <div class="text-h6 text-weight-bold text-white">
-            {{ currentInternship.title }}
-          </div>
-          <div class="text-subtitle1 text-weight-bold q-mt-sm text-white">
-            Тип практики: {{ currentInternship.type }}
-          </div>
-          <div class="text-subtitle1 text-weight-bold text-white">
-            Дата: {{ currentInternship.dates?.start }} - {{ currentInternship.dates?.end }}
-          </div>
+    <template v-else>
+      <q-banner class="primary-background q-pa-md q-my-md rounded-borders">
+        <div class="text-h5 text-weight-bold text-white text-center">
+          Сейчас у вас нет активной практики
+        </div>
+        <div class="text-subtitle1 text-weight-bold text-white text-center">
+          Как только вы договоритесь с компанией, тут появится активная практика
+        </div>
+      </q-banner>
+    </template>
 
-          <div class="row items-center text-white">
-            <q-checkbox
-              v-model="currentInternship.status.approvedCompany"
-              label="Одобрено компанией"
-              color="primary"
-              class="no-pointer-events"
-            />
-            <q-checkbox
-              v-model="currentInternship.status.approvedTeacher"
-              label="Одобрено преподавателем"
-              color="primary"
-              class="no-pointer-events"
-            />
-          </div>
-
-          <div class="q-mt-xs">
-            <div v-if="!currentInternship.status.approvedTeacher" class="row q-gutter-xs">
-              <q-btn
-                label="Загрузить индивидуальное задание"
-                color="white"
-                text-color="primary"
-                unelevated
-                class="text-weight-bold"
-              />
-              <q-btn
-                label="Скачать шаблон индивидуального задания"
-                color="white"
-                text-color="primary"
-                unelevated
-                class="text-weight-bold"
-              />
-            </div>
-
-            <div v-else class="row q-gutter-xs">
-              <div v-if="currentInternship.status.grade === null" class="col-12">
-                <q-input
-                  v-model="currentInternship.status.reportUrl"
-                  label="Ссылка на отчёт (Яндекс Диск)"
-                  filled
-                  dense
-                  bg-color="white"
-                  color="primary"
-                  hide-bottom-space
-                  label-color="primary"
-                  style="max-width: 400px"
-                  class="q-mb-sm"
-                />
-                <q-btn
-                  color="white"
-                  text-color="primary"
-                  unelevated
-                  class="text-weight-bold"
-                  label="Отправить отчёт"
-                />
-              </div>
-
-              <div v-else-if="currentInternship.status.grade <= 2" class="col-12">
-                <div class="text-body1 q-mb-md text-white text-weight-bold">
-                  Оценка за практику: {{ currentInternship.status.grade }}
-                </div>
-                <q-input
-                  v-model="currentInternship.status.reportUrl"
-                  label="Ссылка на отчёт (Яндекс Диск)"
-                  filled
-                  dense
-                  bg-color="white"
-                  color="primary"
-                  hide-bottom-space
-                  label-color="primary"
-                  style="max-width: 400px"
-                  class="q-mb-sm"
-                />
-                <q-btn
-                  color="white"
-                  text-color="primary"
-                  unelevated
-                  class="text-weight-bold"
-                  label="Переотправить отчёт"
-                />
-              </div>
-
-              <div v-else class="col-12">
-                <div class="text-body1 q-mb-md text-white text-weight-bold">
-                  Оценка за практику: {{ currentInternship.status.grade }}
-                </div>
-                <div class="row q-gutter-xs">
-                  <q-btn
-                    v-for="btn in [
-                      'Загрузить индивидуальное задание',
-                      'Загрузить характеристику',
-                      'Загрузить дневник',
-                      'Загрузить отчёт',
-                    ]"
-                    :key="btn"
-                    color="white"
-                    text-color="primary"
-                    unelevated
-                    class="text-weight-bold"
-                    :label="btn"
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-        </q-card-section>
-
-        <q-card-section class="col-12 col-md-3 text-right">
-          <div class="row items-center justify-end q-gutter-sm">
-            <div class="text-h6 text-weight-bold text-white">{{ currentInternship.company }}</div>
-            <q-avatar size="40px" color="white"></q-avatar>
-          </div>
-          <div class="q-mt-sm">
-            <div class="subtitle-1 text-weight-bold text-white">
-              Преподаватель: {{ currentInternship.teacher }}
-            </div>
-          </div>
-          <q-btn
-            label="Чат"
-            class="q-mt-md text-weight-bold"
-            color="white"
-            text-color="primary"
-            unelevated
-          />
-        </q-card-section>
-      </div>
-    </q-card>
-
-    <div class="text-h4 q-mb-sm">Прошлые</div>
-
-    <q-card
-      v-for="past in pastInternships"
-      :key="past.id"
-      flat
-      bordered
-      class="border-radius-md q-mb-sm primary-background text-white"
-    >
-      <div class="row justify-between">
-        <q-card-section class="col-12 col-md-8">
-          <div class="text-h6 text-weight-bold">{{ past.title }}</div>
-          <div class="text-subtitle1 text-weight-bold q-mt-sm">Тип практики: {{ past.type }}</div>
-          <div class="text-body1 text-weight-bold">Оценка за практику: {{ past.status.grade }}</div>
-          <div class="text-subtitle1 text-weight-bold">
-            Дата: {{ past.dates?.start }} - {{ past.dates?.end }}
-          </div>
-
-          <div class="row q-gutter-xs q-mt-sm">
-            <q-btn
-              v-for="btn in [
-                'Скачать индивидуальное задание',
-                'Скачать характеристику',
-                'Скачать дневник',
-                'Скачать отчёт',
-              ]"
-              :key="btn"
-              color="white"
-              text-color="primary"
-              unelevated
-              class="text-weight-bold"
-              :label="btn"
-            />
-          </div>
-        </q-card-section>
-
-        <q-card-section class="col-12 col-md-3 text-right">
-          <div class="row items-center justify-end q-gutter-sm">
-            <div class="text-h6 text-weight-bold text-white">{{ past.company }}</div>
-            <q-avatar size="40px" color="white"></q-avatar>
-          </div>
-          <div class="q-mt-sm">
-            <div class="subtitle-1 text-weight-bold text-white">
-              Преподаватель: {{ past.teacher }}
-            </div>
-          </div>
-        </q-card-section>
-      </div>
-    </q-card>
+    <div class="text-h4 q-mb-sm text-weight-bold">Прошлые</div>
+    <PreviousInternship v-for="item in previousInternships" :key="item.id" :internship="item" />
   </q-page>
 </template>
 
 <script setup lang="ts">
 import { reactive } from 'vue';
+import CurrentInternship from 'src/components/student/CurrentInternship.vue';
+import PreviousInternship from 'src/components/student/PreviousInternship.vue';
+import type { StudentInternship } from 'src/types';
 
-type InternshipStatus = {
-  approvedCompany: boolean;
-  approvedTeacher: boolean;
-  grade: number | null;
-  reportUrl?: string;
-};
-
-type Internship = {
-  id: number;
-  title: string;
-  company: string;
-  image: string;
-  type: string;
-  teacher: string;
-  specialities: string[];
-  skills: string[];
-  status: InternshipStatus;
-  dates: {
-    start: string;
-    end: string;
-  };
-};
-
-const currentInternship = reactive<Internship>({
+const currentInternship = reactive<StudentInternship>({
   id: 1,
   title: 'Разработка микросервиса на FastAPI',
   company: 'Рога и Копыта',
@@ -243,7 +50,7 @@ const currentInternship = reactive<Internship>({
   },
 });
 
-const pastInternships = reactive<Internship[]>([
+const previousInternships = reactive<StudentInternship[]>([
   {
     id: 2,
     title: 'Учебная практика',
