2dfa822bde062c206643c2701720b6ba0b1e315c
densi <ignatiev1904.ph@mail.ru>
2026-03-25 19:19:52 +0800

    adding api data

--- src/pages/CompanyPractice.vue
+++ src/pages/CompanyPractice.vue
@@ -1,25 +1,37 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
+const activeTab = ref('about')
+import { getContacts, getInternshipData, getCandidates, approveRequest, declineRequest, closeInternship } from '../api/companies/index'
+import type { Contact, Internship, Candidate } from '../api/companies/index'
 
-const activeTab = ref('about') 
+
+
+const candidates = ref<Candidate[]>([])
+const contacts = ref<Contact[]>([])
+const internship_data = ref<Internship>()
+
+
+
+
+onMounted(async () => {
+  const contactsResponse = await getContacts()
+  contacts.value = contactsResponse.value
+  const candidatesResponse = await getCandidates()
+  candidates.value = candidatesResponse.value
+  const internshipResponse = await getInternshipData()
+  internship_data.value = internshipResponse.value
+})
 </script>
 
 <template>
   <div class="q-pa-md">
     <div class="row">
-       <div class="col-9">
+      <div class="col-9">
         <q-card flat bordered>
           <q-card-section>
-            <div class="text-h5 q-mb-md text-center">ТЫРЫРЫРЫ КАКАЯ-ТО ПРАКТИКА</div>
-            <q-tabs
-              v-model="activeTab"
-              dense
-              class="text-grey"
-              active-color="primary"
-              indicator-color="primary"
-              align="justify"
-              narrow-indicator
-            >
+            <div class="text-h5 q-mb-md text-center">{{ internship_data?.title }}</div>
+            <q-tabs v-model="activeTab" dense class="text-grey" active-color="primary" indicator-color="primary"
+              align="justify" narrow-indicator>
               <q-tab name="about" label="О практике" />
               <q-tab name="participants" label="Участники" />
               <q-tab name="applications" label="Заявки" />
@@ -30,50 +42,66 @@ const activeTab = ref('about')
             <q-tab-panels v-model="activeTab" animated>
               <q-tab-panel name="about">
                 <div class="text-h6">О практике</div>
-                <p>ВЫ БУДЕТЕ ГОНЯТЬ ЧАИ В ОФИСЕ И ТЫКАТЬ КНОПКИ</p>
+                <p>{{ internship_data?.description }}</p>
               </q-tab-panel>
 
               <q-tab-panel name="participants">
                 <div class="text-h6">Участники</div>
                 <q-list bordered separator>
-                  <q-item>
-                    <q-item-section>Иван Иванов</q-item-section>
-                    <q-item-section side>Активен</q-item-section>
-                  </q-item>
-                  <q-item>
-                    <q-item-section>Петр Петров</q-item-section>
-                    <q-item-section side>Активен</q-item-section>
-                  </q-item>
+                  <template v-for="candidate_item in candidates" :key="candidate_item.id">
+                    <q-item v-if="candidate_item.status === 1">
+                      <q-item-section>
+                        {{ candidate_item.student_name }}
+                      </q-item-section>
+                    </q-item>
+                  </template>
                 </q-list>
               </q-tab-panel>
 
               <q-tab-panel name="applications">
                 <div class="text-h6">Заявки</div>
                 <q-list bordered separator>
-                  <q-item>
-                    <q-item-section>АННА СМИРНОВА ХОЧЕТ РАБОТАТЬ</q-item-section>
-                  </q-item>
+                  <template v-for="candidate_item in candidates" :key="candidate_item.id">
+                    <q-item v-if="candidate_item.status != 1">
+                      <q-item-section>
+                        {{ candidate_item.student_name }}
+                      </q-item-section>
+                      <q-item-section side>
+                        <div class="row q-gutter-sm">
+                          <q-btn @click="approveRequest(candidate_item.id)">
+                            Принять
+                          </q-btn>
+                          <q-btn @click="declineRequest(candidate_item.id)">
+                            Отклонить
+                          </q-btn>
+                        </div>
+                      </q-item-section>
+                    </q-item>
+                  </template>
                 </q-list>
               </q-tab-panel>
             </q-tab-panels>
 
             <div class="row items-center justify-between q-mt-md">
-              <q-btn label="Закрыть набор" color="red" class="q-px-lg" unelevated />
+              <q-btn @click="closeInternship(internship_data?.id)" label="Закрыть набор" color="red" class="q-px-lg"
+                unelevated />
               <q-btn label="Пригласить" color="primary" class="q-px-lg" unelevated />
             </div>
           </q-card-section>
         </q-card>
       </div>
-      <div class="col-3">
+      <div v-for="contactItem in contacts" :key="contactItem.id" class="col-3">
         <q-card>
           <q-card-section>
-            <q-avatar size="100px" color="primary" text-color="white">
-              <q-icon name="person" size="40px" />
-            </q-avatar>
+            <div class="flex justify-center">
+              <q-avatar size="100px" color="primary" text-color="white">
+                <q-icon name="person" size="40px" />
+              </q-avatar>
+            </div>
             <div class="text-h5 q-mt-md text-center">Контакты:</div>
-            <div class="text-h5 q-mt-md text-center">ФИО</div>
-            <div class="text-h5 q-mt-md text-center">Телеграм:</div>
-            <div class="text-h5 q-mt-md text-center">Почта:</div>
+            <div class="text-h5 q-mt-md text-center">{{ contactItem.full_name }}</div>
+            <div class="text-h5 q-mt-md text-center">Телеграм:{{ contactItem.value }}</div>
+            <div class="text-h5 q-mt-md text-center">Почта:{{ contactItem.value }}</div>
           </q-card-section>
         </q-card>
       </div>
