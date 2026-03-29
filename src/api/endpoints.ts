38670f86ffe58cf7c536beec9060c0464b79c1d8
densi <ignatiev1904.ph@mail.ru>
2026-03-29 14:42:59 +0800

    add api endpoints

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -2,9 +2,18 @@ const ENDPOINTS = {
   companies: {
     companies: () => 'companies/companies/',
     byId: (id: number) => `companies/companies/${id}/`,
+    contacts:{
+      contacts:()=>`/companies/contacts/`
+    }
   },
   internships: {
     base: () => 'internship/',
+    byId: (id:number) => `/internship/${id}/`,
+  },
+  requests: {
+    requests:() => `/internship/requests/`,
+    approve: (id: number) => `internship/requests/${id}/approve/`,
+    decline: (id: number) => `internship/requests/${id}/`,
   },
   auth: {
     login: () => 'auths/dev-auth/login/',
